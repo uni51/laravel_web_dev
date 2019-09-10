a4789d2a1a78bb4f1b9efa85824ffd42e1a033e7
uni51 <uni51@users.noreply.github.com>
2019-09-11 07:04:07 +0900

    5-3 Eloquent

--- app/Author.php
+++ app/Author.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
-    //
+    use SoftDeletes; // 論理削除用に追加する
+
+    /*
+     * 著者は複数の書籍レコードを持つ
+     */
+    public function books()
+    {
+        return $this->hasMany('\App\Book');
+    }
 }
--- app/Bookdetail.php
+++ app/Bookdetail.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bookdetail extends Model
 {
-    //
+    /**
+     * 書籍詳細は1つの書籍レコードと紐づく
+     */
+    public function book()
+    {
+        return $this->belongsTo('\App\Book');
+    }
 }
